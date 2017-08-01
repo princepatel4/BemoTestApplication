310cacf2444d6fb7d2a26b11361106c966b6b616
Prince Patel <princempatel04@gmail.com>
2017-08-01 17:48:25 +0530

    Add files via upload

---
+++ app/src/main/java/com/test/bemoapplication/view/ChatConversationActivity.java
@@ -0,0 +1,15 @@
+package com.test.bemoapplication.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.test.bemoapplication.R;
+
+public class ChatConversationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_conversation);
+    }
+}
